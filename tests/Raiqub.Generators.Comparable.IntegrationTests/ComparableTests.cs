555ec6e9b7e3fea0f211f1bca92867bbe5bacac9
Fabricio Godoy <skarllot@gmail.com>
2022-11-30 12:31:23 -0300

    Increase integration test coverage

--- tests/Raiqub.Generators.Comparable.IntegrationTests/ComparableTests.cs
+++ tests/Raiqub.Generators.Comparable.IntegrationTests/ComparableTests.cs
@@ -19,4 +19,56 @@ public class ComparableTests
         (circle1 >= circle2).Should().BeTrue();
         (circle1 <= circle2).Should().BeTrue();
     }
+
+    [Fact]
+    public void Given1CircleSmallerThanOtherThenComparisonReturnsExpectedResult()
+    {
+        var circle1 = new Circle { Radius = 1f };
+        var circle2 = new Circle { Radius = 2f };
+
+        circle1.CompareTo(circle2).Should().Be(-1);
+        (circle1 == circle2).Should().BeFalse();
+        (circle1 != circle2).Should().BeTrue();
+        (circle1 > circle2).Should().BeFalse();
+        (circle1 < circle2).Should().BeTrue();
+        (circle1 >= circle2).Should().BeFalse();
+        (circle1 <= circle2).Should().BeTrue();
+    }
+
+    [Fact]
+    public void Given1CircleBiggerThanOtherThenComparisonReturnsExpectedResult()
+    {
+        var circle1 = new Circle { Radius = 10f };
+        var circle2 = new Circle { Radius = 5f };
+
+        circle1.CompareTo(circle2).Should().Be(1);
+        (circle1 == circle2).Should().BeFalse();
+        (circle1 != circle2).Should().BeTrue();
+        (circle1 > circle2).Should().BeTrue();
+        (circle1 < circle2).Should().BeFalse();
+        (circle1 >= circle2).Should().BeTrue();
+        (circle1 <= circle2).Should().BeFalse();
+    }
+
+    [Fact]
+    public void Given1NullCircleThenComparisonReturnsExpectedResult()
+    {
+        var circle1 = new Circle { Radius = 10f };
+        Circle? circle2 = null;
+
+        circle1.CompareTo(circle2).Should().Be(1);
+        (circle1 == circle2).Should().BeFalse();
+        (circle1 != circle2).Should().BeTrue();
+        (circle1 > circle2).Should().BeTrue();
+        (circle1 < circle2).Should().BeFalse();
+        (circle1 >= circle2).Should().BeTrue();
+        (circle1 <= circle2).Should().BeFalse();
+
+        (circle2 == circle1).Should().BeFalse();
+        (circle2 != circle1).Should().BeTrue();
+        (circle2 > circle1).Should().BeFalse();
+        (circle2 < circle1).Should().BeTrue();
+        (circle2 >= circle1).Should().BeFalse();
+        (circle2 <= circle1).Should().BeTrue();
+    }
 }
